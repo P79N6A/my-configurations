3458fbea3ebfd0287ddfd2a05ecc0c8ea24f380b
ChrisZou <zoumengdie@gmail.com>
2016-10-27 14:39:55 +0800

    Backup existing configs

--- setup.rb
+++ setup.rb
@@ -1,8 +1,16 @@
 #!/usr/bin/env ruby -w
+require 'date'
 
 links = %w[zshrc vimrc gitconfig bin tmux.conf]
 links.each do |link|
+  target = "#{Dir.home}/.#{link}"
+  if File.exist? target
+    cmd = "mv #{target} #{target}_back_chriszou_#{Date.today.to_s}"
+    puts cmd
+    system cmd
+  end
   cmd = "ln -s #{Dir.pwd}/#{link} #{Dir.home}/.#{link}"
   puts cmd
   system(cmd)
+  puts ''
 end
